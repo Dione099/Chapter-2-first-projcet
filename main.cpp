f7d440bc1840c845819ffd07f6d7231b0a6b5535
Dione099 <dionegeorge.dg@gmail.com>
2024-09-10 14:20:41 +0000

    cleared out main.cpp

--- main.cpp
+++ main.cpp
@@ -1,13 +1,8 @@
-// display my initals//
+// 
 #include <iostream>
 using namespace std;
 
 int main() 
 {
-  char first, middle, last;
-first='d';
-middle='v';
-  last='g';
-  cout << first << middle << last << endl;
-  return 0; 
+  c
 }
