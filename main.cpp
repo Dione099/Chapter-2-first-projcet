d7e45497d6ab909a40840aa890099523fd83a32f
Dione099 <dionegeorge.dg@gmail.com>
2024-09-17 14:12:33 +0000

    ran program and was successful

--- main.cpp
+++ main.cpp
@@ -6,6 +6,14 @@
 using namespace std;
 
 int main() 
-{
-  
+{//storing the information in sepreate string variables
+  std::string name = "Dione George";
+  std::string address = "123 Main Street";
+  std::string phone_number = "123-456-7890";
+
+  //display the contents of the string objects
+ std:: cout << "name: " << name << std::endl;
+ std:: cout << "address: " << address << std::endl;
+  std::cout << "phone number: " << phone_number << std::endl;
+  return 0; 
 }
